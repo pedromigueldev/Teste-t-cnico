453e2be6585220205eae4574a3110959db9fcb88
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2024-09-07 13:20:48 -0300

    feat: Init repo, Fibonacci done.

---
+++ 1. Fibonacci/src/Main.kt
@@ -0,0 +1,32 @@
+fun fibonacci(number: Int): Int {
+    if (number <= 1) return number
+
+    return fibonacci(number - 1) + fibonacci(number - 2)
+}
+
+fun main() {
+    println("Type a Number: ")
+    var userInput: String = readln()
+    var isNumberInFibonacci = false
+    var reqNumber = userInput.toIntOrNull()
+
+    while (userInput == "" || reqNumber == null) {
+        println("Please provide a valid Number: ")
+        userInput = readln()
+        reqNumber = userInput.toIntOrNull()
+    }
+
+    for (i in 0..reqNumber + 1){
+        val current = fibonacci(i)
+        print("${current}, ")
+
+        isNumberInFibonacci = current == reqNumber
+        if (current > reqNumber) break
+    }
+
+    if (isNumberInFibonacci)(
+            println("The number $reqNumber is in Fibonacci.")
+    ) else (
+            println("There's no $reqNumber in the Fibonacci sequence.")
+    )
+}
