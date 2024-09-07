75f3afbad5a2af521e7ff54a6f5b802f35d0da2e
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2024-09-07 14:00:45 -0300

    2. Verify String

--- 2. Verificar String/src/Main.kt
+++ 2. Verificar String/src/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val input = "Maria é casada";
+    val input = "Maria é casada"
     val countedAs = input.lowercase().count { it == 'a'}
-    print("There is a total of $countedAs A's in the following text:\n$input");
+    print("There is a total of $countedAs A's in the following text:\n$input")
 }
